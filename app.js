2adb5fb829f45ecb305a08db0eacfc6fb7417a09
sratanota <89356791+sratanota@users.noreply.github.com>
2023-04-19 11:34:44 +0700

    change how to send respond

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express")
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//    user: 'db_486example_user',
+//    host: 'dpg-cggkfk02qv28tc48fmk0-a.singapore-postgres.render.com',
+//    database: 'db_486example',
+//    password: '9N2KSOdKB4W8CADTodmTWPjhp2Ks7Riw',
+//    port: 5432,
+// })
 
 const pgp = require('pg-promise')(/* options */)
 const db = pgp('postgres://db_486example_user:9N2KSOdKB4W8CADTodmTWPjhp2Ks7Riw@dpg-cggkfk02qv28tc48fmk0-a/db_486example')
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/cat', (req, res) => {
-  res.send('Accept Cat Request. Yeh')
+  const { color, region } = req.query;
+  res.send(`Accept Cat Request. Need color = ${color} in ${region}`)
 })
+
+app.get('/cat/:subPath', (req, res) => {
+  const { subPath } = req.params;
+  res.send(`Accept Cat ${subPath} Sub Request.`)
+})
+
+// app.get('/students', (req, res) => {
+//   pool.query('SELECT * from public.student', (error, results) =>{
+//     if (error) { throw error }
+
+//     console.log('DATA:', results.rows)
+//     res.json(results.rows)
+
+//   })
+// })
 
 app.get('/students', (req, res) => {
   db.any('SELECT * from public.student')
-  .then((data) => {
-    console.log('DATA:', data)
-    res.send(JSON.stringify(data))
-  })
-  .catch((error) => {
-    console.log('ERROR:', error)
-    res.send ("ERROR:Can't get data")
-  })
-  
+    .then((data) => {
+      console.log('DATA:', data)
+      res.json(data)
+    })
+    .catch((error) => {
+      console.log('ERROR:', error)
+      res.send("ERROR:Can't get data")
+    })
+
 })
+
 
 app.listen(port, () => {
   console.log(`My Example app listening on port ${port}`)
